7e25a12cf319490bc5696249e228224bfcb19a9e
ianlam323 <igchkshop@gmail.com>
2025-12-05 21:25:47 +0000

    Create schema.sql

---
+++ worker/schema.sql
@@ -0,0 +1,18 @@
+-- USERS TABLE
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    verified INTEGER DEFAULT 0,
+    verification_token TEXT,
+    reset_token TEXT,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP
+);
+
+-- ADMIN TABLE
+CREATE TABLE IF NOT EXISTS admins (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER UNIQUE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
